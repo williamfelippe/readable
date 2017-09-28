66a269d72c9c72b48b70ab9ac947138bc9a2ddb9
William Felippe F. da Silva <wfelippesilva@gmail.com>
2017-09-28 01:13:21 -0300

    feat: Adding LinkButton component

---
+++ src/components/Spectre/LinkButton.jsx
@@ -0,0 +1,59 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { NavLink } from 'react-router-dom'
+
+const LinkButton = ({
+    to,
+    children,
+    className,
+    disabled,
+    kind,
+    size,
+    loading,
+    exact,
+    external
+}) => {
+
+    let link = null
+
+    if (external) {
+        link = <a href={to} target="_blank" className={`btn ${className} ${disabled ? 'disabled' : ''}
+                ${size ? `btn-${size}` : ''} ${kind ? `btn-${kind}` : ''} 
+                ${loading ? 'loading' : ''}`}>
+            {children}
+        </a>
+    }
+    else {
+        link = <NavLink to={to} exact={exact} className={`btn ${className} ${disabled ? 'disabled' : ''}
+                ${size ? `btn-${size}` : ''} ${kind ? `btn-${kind}` : ''} 
+                ${loading ? 'loading' : ''}`}>
+            {children}
+        </NavLink>
+    }
+
+    return link
+}
+
+LinkButton.defaultProps = {
+    className: '',
+    disabled: false,
+    kind: null,
+    size: null,
+    loading: false,
+    exact: false,
+    external: false
+}
+
+LinkButton.propTypes = {
+    to: PropTypes.string.isRequired,
+    children: PropTypes.node.isRequired,
+    kind: PropTypes.oneOf(['primary', 'link']),
+    size: PropTypes.oneOf(['lg', 'sm', 'block']),
+    className: PropTypes.string,
+    disabled: PropTypes.bool,
+    loading: PropTypes.bool,
+    exact: PropTypes.bool,
+    external: PropTypes.bool
+}
+
+export default LinkButton
--- src/components/Spectre/index.js
+++ src/components/Spectre/index.js
@@ -2,6 +2,7 @@ import Container from './Container'
 import Columns from './Columns'
 import Col from './Col'
 import Button from './Button'
+import LinkButton from './LinkButton'
 import Input from './Input'
 import TextArea from './TextArea'
 import Select from './Select'
@@ -12,6 +13,7 @@ export {
     Columns,
     Col,
     Button,
+    LinkButton,
     Input,
     TextArea,
     Select,
