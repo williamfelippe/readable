b3866116b4b1421e0e48cd73663473d14239cbdb
williamfelippe <wfelippesilva@gmail.com>
2017-11-23 00:04:44 -0200

    feat: Adding actions and validation for a deleted post

--- client/src/containers/PostDetail/PostDetail.jsx
+++ client/src/containers/PostDetail/PostDetail.jsx
@@ -1,83 +1,139 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { CommentsList } from '../../components/Comments'
-import { Container, Columns, Col, Button, Icon } from '../../components/Spectre'
-import { posts as postsActions, comments as commentsActions } from '../../actions'
+import { PostActions, PostDeleted } from '../../components/Posts'
 import { UP_VOTE, DOWN_VOTE } from '../../constants/voteTypes'
+import {
+    posts as postsActions,
+    comments as commentsActions
+} from '../../actions'
+import {
+    Container,
+    Columns,
+    Col,
+    Button,
+    Icon
+} from '../../components/Spectre'
 import formatDate from '../../utils/formatDate'
 import './style.css'
 
 class PostDetail extends Component {
+
+    constructor(props) {
+        super(props)
+        this.state = {
+            postDeleted: false
+        }
+    }
 
     componentDidMount() {
         const { getPost, getComments, match } = this.props
         const { postId } = match.params
 
         getPost(postId)
-        getComments(postId)
+            .then((post) => {
+                console.log('POST', post)
+
+                if (!post) {
+                    this.setState({ postDeleted: true })
+                    return
+                }
+
+                this.setState({ postDeleted: false })
+                getComments(postId)
+            })
+    }
+
+    renderElements() {
+        const { votePost, match, post, commentsLength } = this.props
+        const { postDeleted } = this.state
+        const { postId } = match.params
+
+        if (postDeleted) {
+            return <PostDeleted />
+        }
+
+        if (post) {
+            return (
+                <Container grid="md">
+                    <Columns>
+                        <Col xs={12}>
+                            <div className="postDetail">
+                                <h1>
+                                    {post.title} <span className="label label-primary postDetail__category">
+                                        {post.category}
+                                    </span>
+                                </h1>
+
+                                <h4>
+                                    Posted by {post.author} <span className="postDetail__date">
+                                        {formatDate(post.timestamp)}
+                                    </span>
+                                </h4>
+
+                                <div className="postDetail__info">
+                                    <p className="postDetail__info__body">
+                                        {post.body}
+                                    </p>
+
+                                    <div className="postDetail__info__voteButtons">
+                                        <Button
+                                            kind="link"
+                                            className="postDetail__info__voteButtons__voteIcon"
+                                            onClick={() => votePost(postId, UP_VOTE)}>
+                                            <Icon icon="arrow-up" />
+                                        </Button>
+
+                                        <p className="postDetail__info__voteButtons__voteScore">
+                                            {post.voteScore}
+                                        </p>
+
+                                        <Button
+                                            kind="link"
+                                            className="postDetail__info__voteButtons__voteIcon"
+                                            onClick={() => votePost(postId, DOWN_VOTE)}>
+                                            <Icon icon="arrow-down" />
+                                        </Button>
+                                    </div>
+                                </div>
+
+                                <div className="postDetail__actions">
+                                    <PostActions
+                                        postId={postId}
+                                        commentsLength={commentsLength}
+                                        withCategory={false} />
+                                </div>
+
+                                <CommentsList postId={postId} />
+                            </div>
+                        </Col>
+                    </Columns>
+                </Container>
+            )
+        }
+        
+        return null
     }
 
     render() {
-        const { votePost, match, post } = this.props
-        const { postId } = match.params
-
         return (
-            (post) ? <Container grid="md">
-                <Columns>
-                    <Col xs={12}>
-                        <div className="postDetail">
-                            <h1>
-                                {post.title} <span className="label label-primary postDetail__category">
-                                    {post.category}
-                                </span>
-                            </h1>
-
-                            <h4>
-                                Posted by {post.author} <span className="postDetail__date">
-                                    {formatDate(post.timestamp)}
-                                </span>
-                            </h4>
-
-                            <div className="postDetail__info">
-                                <p className="postDetail__info__body">
-                                    {post.body}
-                                </p>
-
-                                <div className="postDetail__info__voteButtons">
-                                    <Button 
-                                        kind="link" 
-                                        className="postDetail__info__voteButtons__voteIcon"
-                                        onClick={() => votePost(postId, UP_VOTE)}>
-                                        <Icon icon="arrow-up" />
-                                    </Button>
-
-                                    <p className="postDetail__info__voteButtons__voteScore">
-                                        {post.voteScore}
-                                    </p>
-
-                                    <Button 
-                                        kind="link" 
-                                        className="postDetail__info__voteButtons__voteIcon"
-                                        onClick={() => votePost(postId, DOWN_VOTE)}>
-                                        <Icon icon="arrow-down" />
-                                    </Button>
-                                </div>
-                            </div>
-
-                            <CommentsList postId={postId} />
-                        </div>
-                    </Col>
-                </Columns>
-            </Container> : null
+            this.renderElements()
         )
     }
 }
 
-const mapStateToProps = ({ posts }, {match}) => {
+const mapStateToProps = ({ posts, comments }, { match }) => {
     const { postId } = match.params
+    const commentsByPost = comments.comments[postId]
+
+    const commentsLength =
+        (commentsByPost && commentsByPost !== undefined)
+            ? Object.values(commentsByPost).length
+            : 0
 
     return {
-        post: posts.posts[postId]
+        post: posts.posts[postId],
+        commentsLength
     }
 }
 
